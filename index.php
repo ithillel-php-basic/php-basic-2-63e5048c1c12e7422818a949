cfb1bdfb296afc3cf1b764f9e6bdbc684ef497b8
volodkaly <lysvl@hotmail.cz>
2023-05-14 11:10:15 +0200

    task 6 attempt

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php
 
 include "helpers.php";
@@ -16,80 +15,113 @@ $categories = [
 
 $tasks = [
     [
-        'title'=>'замовити піцу',
-        'date_realisation'=>'04.06.2023',
-        'category'=>'Домашні справи',
-        'status'=>'to-do',
+        'title' => 'замовити піцу',
+        'date_realisation' => '13.05.2023',
+        'category' => 'Домашні справи',
+        'status' => 'to-do',
     ],
     [
-        'title'=>'вивчити цейво',
-        'date_realisation'=>'04.06.2023',
-        'category'=>'Навчання',
-        'status'=>'to-do',
+        'title' => 'вивчити цейво',
+        'date_realisation' => '14.05.2023',
+        'category' => 'Навчання',
+        'status' => 'to-do',
     ],
     [
-        'title'=>'погуляти',
-        'date_realisation'=>'02.06.2023',
-        'category'=>'Домашні справи',
-        'status'=>'to-do',
+        'title' => 'нагодувати кота',
+        'date_realisation' => '15.05.2023',
+        'category' => 'Домашні справи',
+        'status' => 'to-do',
     ],
     [
-        'title'=>'погуляти',
-        'date_realisation'=>'02.06.2023',
-        'category'=>'Домашні справи',
-        'status'=>'to-do',
+        'title' => 'погладити кота',
+        'date_realisation' => '15.05.2023',
+        'category' => 'Домашні справи',
+        'status' => 'to-do',
     ],
     [
-        'title'=>'написати сайт',
-        'date_realisation'=>'01.02.2021',
-        'category'=>'Робота',
-        'status'=>'done',
+        'title' => 'погуляти',
+        'date_realisation' => '02.06.2023',
+        'category' => 'Домашні справи',
+        'status' => 'to-do',
     ],
     [
-        'title'=>'пройти співбесіду',
-        'date_realisation'=>'01.02.2015',
-        'category'=>'Робота',
-        'status'=>'backlog',
+        'title' => 'написати сайт',
+        'date_realisation' => '01.06.2023',
+        'category' => 'Робота',
+        'status' => 'in-progress',
     ],
     [
-        'title'=>'пройти співбесіду',
-        'date_realisation'=>'01.02.2015',
-        'category'=>'Робота',
-        'status'=>'backlog',
+        'title' => 'пограти в козаки 3',
+        'date_realisation' => '01.07.2022',
+        'category' => 'Робота',
+        'status' => 'done',
     ],
     [
-        'title'=>'знайти вхід',
-        'date_realisation'=>'01.02.2011',
-        'category'=>'Вхідні',
-        'status'=>'backlog',
+        'title' => 'пройти співбесіду',
+        'date_realisation' => '01.08.2023',
+        'category' => 'Робота',
+        'status' => 'backlog',
+    ],
+    [
+        'title' => 'знайти вхід',
+        'date_realisation' => null,
+        'category' => 'Вхідні',
+        'status' => 'backlog',
+    ],
+    [
+        'title' => 'винести сміття',
+        'date_realisation' => '01.01.2023',
+        'category' => 'Вхідні',
+        'status' => 'done',
+    ],
+    [
+        'title' => 'помити посуд',
+        'date_realisation' => '01.09.2023',
+        'category' => 'Вхідні',
+        'status' => 'in-progress',
     ],
 ];
 
-function countHome ($array,$category){
+function countHome($array, $category)
+{
     $i = 0;
-    foreach ($array as $ar){
-        if($ar['category'] === $category){
+    foreach ($array as $ar) {
+        if ($ar['category'] === $category) {
             $i = $i + 1;
         }
     }
     return $i;
 }
 
-$kanban = renderTemplate('kanban.php',[
-    'tasks'=>$tasks,
+function showTime($dateRealisation)
+{
+    if (!is_null($dateRealisation)) {
+        $currentHours = floor(time() / 3600);
+        $timeLeft = floor(strtotime($dateRealisation) / 3600) - $currentHours;
+        if ($timeLeft > 24) {
+            $timeLeft = floor($timeLeft / 24);
+            return $timeLeft . ' днів';
+        }
+        return $timeLeft . ' годин';
+    }
+}
+
+
+$kanban = renderTemplate('kanban.php', [
+    'tasks' => $tasks,
 ]);
 
-$main = renderTemplate('main.php',[
-    'kanban'=>$kanban,
-    'tasks'=>$tasks,
-    'username'=>'Volodymyr',
-    'photopath'=>"static/img/user2-160x160.jpg",
-    'categories'=>$categories,
+$main = renderTemplate('main.php', [
+    'kanban' => $kanban,
+    'tasks' => $tasks,
+    'username' => 'Volodymyr',
+    'photopath' => "static/img/user2-160x160.jpg",
+    'categories' => $categories,
 ]);
 
-echo renderTemplate('layout.php',[
-    'content'=>$main,
-    'pagename'=>$pagename,
+echo renderTemplate('layout.php', [
+    'content' => $main,
+    'pagename' => $pagename,
 ]);
 
 
